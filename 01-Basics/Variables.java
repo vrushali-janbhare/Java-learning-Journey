761fc1fa64cca5a2bec6bdbdd845f147fb3e6da1
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-08-21 09:17:09 +0530

    Add Java Hello world and Variables Programs and ignore class files

---
+++ 01-Basics/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+   public static void main(String[] args) {
+    // Question:
+// Write a Java program to declare and initialize variables to store
+// your name, age, course name, and percentage. Display all the values.
+  String Name="Vrushali";
+  int Age=20;
+  String CourseName ="BCA";
+  float Percentage= 90.18f;
+  System.out.println("Name: "+Name);
+  System.out.println("Age : "+Age);
+  System.out.println("Course Name : "+CourseName);
+  System.out.println("Percentage : "+Percentage);
+   }
+    
+}
