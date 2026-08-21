18edbc3856df4ff3eb86f1c928354dda813547c5
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-08-21 09:48:12 +0530

    Add DataType.java  program

---
+++ 01-Basics/DataTypes.java
@@ -0,0 +1,29 @@
+// Question: Write a Java program to declare variables using different primitive data types such as int,long,short,byte, float, double, char, and boolean, and display their values.
+
+public class DataTypes {
+   public static void main(String[] args) {
+    // Use for store an Interger values 
+    byte By =12;
+    short sh=232;
+    int In=10000;
+    long lg=3000000000l;
+    //Use for store the Decimal Values 
+    float f=3.14f;
+    double d=3.1423455;
+    //Ues for store a Character 
+    char ch='A';
+    // Use for store Boolean (true/false)
+    boolean B=true;
+
+
+    //printing the values 
+    System.out.println("Byte  : "+By);
+    System.out.println("Short : "+sh);
+    System.out.println("Int : "+In);
+    System.out.println("Long : "+lg);
+    System.out.println("Float  : "+f);
+    System.out.println("Double : "+d);
+    System.out.println("charecter : "+ch);
+    System.out.println("Boolean : "+B);
+   } 
+}
