27aaacefc6a8c2f0cfc978e59977f40513c7ab39
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-08-26 08:03:40 +0530

    Loops Print Perpendicular Line

---
+++ 01-Basics/PrintPerpendicularLine.java
@@ -0,0 +1,12 @@
+public class PrintPerpendicularLine {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+         System.out.print("* ");
+        }
+        System.out.println();
+        for(int i=0;i<5;i++){
+                        System.out.println("   *");
+        }
+    }
+}
