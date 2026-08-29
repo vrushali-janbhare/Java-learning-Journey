97147241cba78e294a2e84ec7de5d94cd3c3d70c
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-08-29 14:16:42 +0530

    Create static int square(int n) that returns the square of a number.

---
+++ 01-Basics/SquareofaNumberFunction.java
@@ -0,0 +1,11 @@
+public class SquareofaNumberFunction {
+
+    public static void main(String[] args) {
+        int num=12;
+        int square=square(num);
+        System.out.println("Square of "+num+" is : "+square);
+    }
+    static int square(int n){
+        return n*n;
+    }
+}
