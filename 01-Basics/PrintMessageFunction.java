4b1d127c0dec1d82faacb58358eba76a684f37a3
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-08-29 14:02:42 +0530

    Create a static function greet() that prints "Welcome to Java"

---
+++ 01-Basics/PrintMessageFunction.java
@@ -0,0 +1,8 @@
+public class PrintMessageFunction {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+      System.out.println("Welcome to Java");
+    }
+}
