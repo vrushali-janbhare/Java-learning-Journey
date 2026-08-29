c74ea19db588782a4aca0609f9ea0ab4e4df5d48
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-08-29 14:06:56 +0530

    Create static int add(int a, int b) that returns the sum

---
+++ 01-Basics/AdditionofTwoNumbersFunction.java
@@ -0,0 +1,11 @@
+public class AdditionofTwoNumbersFunction {
+    public static void main(String[] args) {
+         int  x=10;
+         int  y=20;
+         int sum=add(x, y);
+        System.out.println("Sum of "+x+" and "+y+" = "+sum);
+    }
+    static int add(int a, int b){
+        return a+b;
+    }
+}
