cb3a52e6a25879effb269611cfcdda96ffa0651a
vrushali-janbhare <janbharevrushali1826@gmail.com>
2026-09-03 22:00:01 +0530

    code is update

--- OOPS/DisplayMethodOverloaded.java
+++ OOPS/DisplayMethodOverloaded.java
@@ -1,5 +1,20 @@
+// *Create a Display class with overloaded show()
+//  methods to display different types of data.*
+
 public class DisplayMethodOverloaded {
     public static void main(String[] args) {
-        
+        DisplayMethodOverloaded a1=new DisplayMethodOverloaded(); 
+        a1.show("Vrushali");
+       a1.show(20);
+       a1.show(90.18);
+    }
+    void show(int a){
+      System.out.println("Age : "+a);
+    }
+    void show(double a){ 
+     System.out.println("Marks : "+a);
+    }
+    void show(String a){
+      System.out.println("The Name of Student : "+a);
     }
 }
